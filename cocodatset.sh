d7324a98a2541142ddf65e069d7530d70826fc23
2eey10 <coherentman0103@naver.com>
2024-07-09 18:25:57 +0900

    feature: blip2_main setting sh file upload

---
+++ cocodatset.sh
@@ -0,0 +1,5 @@
+pip install aria2
+aria2 http://images.cocodataset.org/zips/val2017.zip
+wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+unzip val2017.zip   
+unzip annotations_trainval2017.zip
